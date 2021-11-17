bb340e6b0a6ba24b80af0e7a1ae81b70062d2b4b
Влад Шипилов <71203042+felocSsS@users.noreply.github.com>
2021-11-17 10:34:09 +0300

    Add files via upload

---
+++ Meditation_Shipilov_Vlad/Meditation_Shipilov_Vlad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meditation_Shipilov_Vlad
+//
+//  Created by Student on 17.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
